4e4a536330fdd57cb056d7b27567414ebed2a970
Alex <alikhanialex@gmail.com>
2019-01-22 15:26:08 +0000

    eventCalendar class can show events

--- src/EventCalendar.js
+++ src/EventCalendar.js
@@ -1,3 +1,16 @@
 class EventCalendar {
-  
+  constructor(eventList = null) {
+    this._eventList = [];
+  }
+
+  pushEvent(event) {
+  this._eventList.push(event);
+}
+
+  showEvents() {
+    for(var i = 0; i < this._eventList.length; i++) {
+      return this._eventList[i];
+    }
+  }
+
 }
