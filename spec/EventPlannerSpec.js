aac93b97056095acc27fce88ffb511de792d46a4
Emanuele <emanuelegorga@gmail.com>
2019-01-22 14:35:07 +0000

    First failing test for creating EventPlanner Class

---
+++ spec/EventPlannerSpec.js
@@ -0,0 +1,9 @@
+describe('EventPlanner', function() {
+
+  let eventplanner = new EventPlanner()
+
+  it('eventPlanner is an instance of Event Planner class', function() {
+    instanceOf(eventPlanner, EventPlanner());
+  });
+
+});
