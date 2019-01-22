10bccbc590f5adccff1ef755ae27b1d11006369e
Alex <alikhanialex@gmail.com>
2019-01-22 15:00:22 +0000

    it now checks for content

--- spec/EventPlannerSpec.js
+++ spec/EventPlannerSpec.js
@@ -5,4 +5,11 @@ describe('EventPlanner', function() {
   it('eventPlanner is an instance of Event Planner class', function() {
     instanceOf(eventPlanner, EventPlanner);
   });
+
+  it('checks for content', function() {
+    let eventPlanner2 = new EventPlanner('content')
+    // console.log(eventPlanner2.getContent())
+    expectToEqual('content', eventPlanner2.getContent());
+  });
+
 });
--- src/EventPlanner.js
+++ src/EventPlanner.js
@@ -1,3 +1,18 @@
 class EventPlanner {
-  
+  constructor(content, time) {
+    this._content = content || null
+    this._time = time || null
+  }
+
+  getContent() {
+    return this._content;
+  }
+
+  getTime() {
+    return this._time;
+  }
+
+
+
+
 }
